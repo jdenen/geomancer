adfaba40424897863ca4bcc3e51c4a8ab582d6c2
Johnson Denen <johnson.denen@gmail.com>
2019-06-06 00:19:49 -0400

    Rough plan with specs

--- lib/geomancer.ex
+++ lib/geomancer.ex
@@ -1,18 +1,70 @@
 defmodule Geomancer do
   @moduledoc """
-  Documentation for Geomancer.
+  TODO Documentation for Geomancer.
   """
 
-  @doc """
-  Hello world.
+  @type reason :: String.t()
 
-  ## Examples
+  defmodule Feature do
+    @type t :: %__MODULE__{
+      type: String.t(),
+      properties: term(),
+      geometry: %{
+        type: String.t(),
+        coordinates: [term()]
+      }
+    }
 
-      iex> Geomancer.hello()
-      :world
+    defstruct type: "Feature",
+              properties: nil,
+              geometry: nil
 
-  """
-  def hello do
-    :world
+    @spec new(geo_type :: String.t(), props :: map(), coords :: [term()]) :: t()
+    def new(geo_type, props, coords) do
+      geo = %{type: geo_type, coordinates: coords}
+      %__MODULE__{properties: props, geometry: geo}
+    end
+  end
+
+  defmodule Object do
+    alias Geomancer.Feature
+
+    @type t :: %__MODULE__{
+            type: String.t(),
+            name: String.t(),
+            features: [Feature.t()]
+          }
+
+    defstruct type: "FeatureCollection",
+              name: nil,
+              features: nil
+
+    @spec new(name :: String.t(), features :: [Feature.t()]) :: t()
+    def new(name, features) do
+      %__MODULE__{name: name, features: features}
+    end
+  end
+
+  defmodule Shapefile do
+    alias Geomancer.{Object, Feature}
+
+    @type reason :: String.t()
+
+    @spec geo_json(String.t()) :: {:ok, Object.t()} | {:error, reason()}
+    def geo_json(_zip_path) do
+      feature = Feature.new("Point", %{}, [10.0, 10.0])
+      object = Object.new("MyPoint", [feature])
+      {:ok, object}
+    end
+  end
+
+  @spec geo_json(String.t()) :: {:ok, Geomancer.Object.t()} | {:error, reason()}
+  def geo_json(input_filepath) do
+    case Path.extname(input_filepath) do
+      ".zip"
+        -> Geomancer.Shapefile.geo_json(input_filepath)
+      ext
+        -> {:error, "Unsupported format: #{ext}"}
+    end
   end
 end
--- test/geomancer_test.exs
+++ test/geomancer_test.exs
@@ -2,7 +2,9 @@ defmodule GeomancerTest do
   use ExUnit.Case
   doctest Geomancer
 
-  test "greets the world" do
-    assert Geomancer.hello() == :world
+  describe "geo_json/1" do
+    test "returns error tuple for unsupported file format" do
+      assert {:error, "Unsupported format: .bar"} = Geomancer.geo_json("foo.bar")
+    end
   end
 end
