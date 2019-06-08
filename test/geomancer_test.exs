dbb96ca15898618739e69fbb56eb283d14789870
Johnson Denen <johnson.denen@gmail.com>
2019-06-08 09:34:18 -0400

    Clean up GeomancerTest module (wip)

--- test/geomancer_test.exs
+++ test/geomancer_test.exs
@@ -2,47 +2,12 @@ defmodule GeomancerTest do
   use ExUnit.Case
   doctest Geomancer
 
-  @point_map %{
-    type: "FeatureCollection",
-    name: "point",
-    features: [
-      %{
-        type: "Feature",
-        properties: %{
-          point_ID: nil
-        },
-        geometry: %{
-          type: "Point",
-          coordinates: [10.0, 10.0]
-        }
-      },
-      %{
-        type: "Feature",
-        properties: %{
-          point_ID: nil
-        },
-        geometry: %{
-          type: "Point",
-          coordinates: [5.0, 5.0]
-        }
-      },
-      %{
-        type: "Feature",
-        properties: %{
-          point_ID: nil
-        },
-        geometry: %{
-          type: "Point",
-          coordinates: [0.0, 10.0]
-        }
-      }
-    ]
-  }
-
   describe "geo_json/1" do
     test "converts simple Shapefile to GeoJSON" do
+      fixture_map = "test/support/point.geojson" |> File.read!() |> Jason.decode!()
       assert {:ok, geo_json} = Geomancer.geo_json("test/support/point.zip")
-      assert geo_json == Jason.encode!(@point_map)
+      geo_map = Jason.decode!(geo_json)
+      assert geo_map == fixture_map
     end
 
     test "converts more complicated Shapefile to GeoJSON" do
