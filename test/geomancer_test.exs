bd948ef61ec192c19e5332b471df793e5a9723c1
Johnson Denen <johnson.denen@gmail.com>
2019-06-14 08:43:53 -0400

    MultiPolygons (wip)

--- test/geomancer_test.exs
+++ test/geomancer_test.exs
@@ -3,16 +3,23 @@ defmodule GeomancerTest do
   doctest Geomancer
 
   describe "geo_json/1" do
-    test "converts simple Shapefile to GeoJSON" do
+    test "converts Shapefile with Points to GeoJSON" do
       fixture_map = "test/support/point.geojson" |> File.read!() |> Jason.decode!()
       assert {:ok, geo_json} = Geomancer.geo_json("test/support/point.zip")
       geo_map = Jason.decode!(geo_json)
       assert geo_map == fixture_map
     end
 
-    test "converts more complicated Shapefile to GeoJSON" do
+    test "converts Shapefile with Polygons to GeoJSON" do
       fixture_map = "test/support/Campus_Boundary.geojson" |> File.read!() |> Jason.decode!()
       assert {:ok, geo_json} = Geomancer.geo_json("test/support/Campus_Boundary.zip")
+      geo_map = Jason.decode!(geo_json)
+      assert geo_map == fixture_map
+    end
+
+    test "converts Shapefile with MultiPolygons to GeoJSON" do
+      fixture_map = "test/support/Urban_Services_Boundary.geojson" |> File.read!() |> Jason.decode!()
+      assert {:ok, geo_json} = Geomancer.geo_json("test/support/Urban_Services_Boundary.zip")
       geo_map = Jason.decode!(geo_json)
       assert geo_map == fixture_map
     end
