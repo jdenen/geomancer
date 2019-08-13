c5a115a03fca4b32cbc1ad87027a3b3cf780319e
Johnson Denen <johnson.denen@gmail.com>
2019-08-13 11:52:51 -0400

    Setup for Hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Geomancer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       aliases: aliases(),
       dialyzer: [plt_file: {:no_warn, "plt/dialyzer.plt"}]
@@ -27,11 +29,25 @@ defmodule Geomancer.MixProject do
       {:jason, "~> 1.1"},
       {:credo, "~> 0.10", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
-      {:placebo, "~> 1.2", only: [:dev, :test]}
+      {:placebo, "~> 1.2", only: [:dev, :test]},
+      {:ex_doc, "~> 0.20.2", only: [:dev]}
     ]
   end
 
   defp aliases() do
     [verify: ["dialyzer", "credo", "format --check-formatted"]]
   end
+
+  defp package do
+    [
+      maintainers: ["Johnson Denen", "Paul Linville"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/jdenen/geomancer"}
+    ]
+  end
+
+  defp description() do
+    "A library to convert geospatial data from format-to-format: Shapefile, KML, CSV, GeoJSON, etc."
+  end
+
 end
